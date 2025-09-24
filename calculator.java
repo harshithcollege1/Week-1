9023aa40f634c58e4277f386e9e4c7ad0a602145
harshithKumar <harshithkumar.cs24@bmsce.ac.in>
2025-09-24 18:58:42 +0530

    Calculator creation

---
+++ calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class calculator{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+
+        int a,b;
+        System.out.print("Enter your First number :");
+        a = scanner.nextInt();
+
+        System.out.print("Enter your Second number :");
+        b = scanner.nextInt();
+
+        System.out.println("Addition :" + (a+b));
+        System.out.println("Substraction :" + (a-b));
+        System.out.println("Multiplication :" + (a*b));
+        System.out.println("Division :" + (float)(a/b));
+    
+        scanner.close();
+    }
+}
